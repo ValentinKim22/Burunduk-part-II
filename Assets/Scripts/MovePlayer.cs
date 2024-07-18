e4eabfeb61f65f1e1fa7eeaf9e6703085226880c
Satyr <Daranmic@gmail.com>
2024-07-18 13:22:43 +0700

    jump

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -6,38 +6,59 @@ public class MovePlayer : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator anim;
-    public SpriteRenderer sr;
-    public Vector2 MoveVector;
+    public Vector2 moveVector;
     public float speed = 1.5f;
-    public bool FaceRight = true;
+    public bool faceRight = true;
+    public float jumpForce = 7f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Walk();
-        Reflect();
+        Walk ();
+        Reflect ();
+        Jump ();
+        CheckingGround ();
     }
     void Walk()
     {
-        MoveVector.x = Input.GetAxis("Horizontal");
-        anim.SetFloat("MoveX", Mathf.Abs(MoveVector.x));
-        rb.velocity = new Vector2(MoveVector.x * speed, rb.velocity.y);
+        moveVector.x = Input.GetAxis("Horizontal");
+        anim.SetFloat("MoveX", Mathf.Abs(moveVector.x));
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
     }
 
     void Reflect()
     {
-        if ((MoveVector.x > 0 && !FaceRight) || (MoveVector.x < 0 && FaceRight))
+        if ((moveVector.x > 0 && !faceRight) || (moveVector.x < 0 && faceRight))
         {
             transform.localScale *= new Vector2(-1, 1);
-            FaceRight = !FaceRight;
+            faceRight = !faceRight;
         }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    public bool onGround;
+    public Transform GroundCheck;
+    public float checkRadius = 0.3f;
+    public LayerMask Ground;
+    void CheckingGround ()
+    {
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, Ground);
+        anim.SetBool("onGround", onGround);
     }
 }
