7fccca9a375e8fb796face08d09d79788de7cea4
Satyr <Daranmic@gmail.com>
2024-07-19 16:10:33 +0700

    enemy

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public bool HeadUp;
+    public Transform HeadCheck;
+    public float checkRadius = 0.5f;
+    public LayerMask Head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DamageEnemy();
+    }
+    void DamageEnemy()
+    {
+        HeadUp = Physics2D.OverlapCircle(HeadCheck.position, checkRadius, Head);
+        if (HeadUp == true )
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
