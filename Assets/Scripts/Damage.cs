d5589dfa4f4039809860976af4496458969e839c
Satyr <Daranmic@gmail.com>
2024-07-22 12:35:21 +0700

    DeatAnim
    Исправил исчезновение объекта до анимации

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    public Animator anim;
+    public bool ChekAnim = true;
     public bool HeadUp;
     public Transform HeadCheck;
-    public float checkRadius = 0.3f;
+    public float checkRadius = 0.5f;
     public LayerMask Head;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,10 +24,12 @@ public class Damage : MonoBehaviour
     void DamageEnemy()
     {
         HeadUp = Physics2D.OverlapCircle(HeadCheck.position, checkRadius, Head);
-        if (HeadUp == true )
-        {
-            Destroy(gameObject);
-        }
-        
+        anim.SetBool("Death", HeadUp);
+        ChekAnim = false;
+    }  
+    void Death()
+    {
+        if (ChekAnim == false)
+        Destroy(gameObject);
     }
 }
